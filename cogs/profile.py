10291f504a9ffc07909508bd65d2954c978e2b87
happiestbee <87628038+happiestbee@users.noreply.github.com>
2021-08-25 03:53:47 -0400

    Started using cass library

--- cogs/profile.py
+++ cogs/profile.py
@@ -1,38 +1,33 @@
 # import packages
 import discord
 from discord.ext import commands
-import requests
-import json
+import cassiopeia as cass
 
-# get api key
+# get api key and set up cass
 with open('api.txt', 'r') as f:
     key = f.read()
+cass.set_riot_api_key(key)
+cass.set_default_region("NA")
 
 
-# main profile cog
+# get summoner data
+async def summoner(ctx, username):
+    user = cass.get_summoner(name=username)
+    if not user.exists:
+        await ctx.send("User not found")
+        return
+    return user.profile_icon.url
+
+
+# profile cog
 class Profile(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+
         # users used to store and discord user and league username in dict
         self.users = {}
 
-    # load summoner data
-    async def summoner(self, ctx, username):
-        url = f"https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/{username}?api_key={key}"
-        data = requests.get(url)
-        json_data = json.loads(data.text)
-        try:
-            user_id = json_data["id"]
-        except KeyError:
-            await ctx.send("User not found")
-            return False
-        icon = f"http://ddragon.leagueoflegends.com/cdn/11.16.1/img/profileicon/{json_data['profileIconId']}.png"
-        level = json_data["summonerLevel"]
-        user = json_data["name"]
-        tier, rank, lp, wins, losses = await self.summoner_data(ctx, user_id)
-        return icon, level, user, tier, rank, lp, wins, losses
-
-    # get summoner data from id
+    '''# get summoner data from id
     async def summoner_data(self, ctx, summoner_id):
         url = f"https://na1.api.riotgames.com/lol/league/v4/entries/by-summoner/{summoner_id}?api_key={key}"
         data = requests.get(url)
@@ -47,7 +42,7 @@ class Profile(commands.Cog):
         wins = json_data["wins"]
         losses = json_data["losses"]
         return tier, rank, lp, wins, losses
-
+'''
     # create profile command
     @commands.command(help='Find the profile of a given player or your linked account',
                       aliases=['p'])
@@ -62,17 +57,19 @@ class Profile(commands.Cog):
                 return
 
         # getting information about user
-        icon, level, user, tier, rank, lp, wins, losses = await self.summoner(ctx, username)
+        icon = await summoner(ctx, username)
+        await ctx.send(icon)
+
+        '''# create profile embed
         profile_embed = discord.Embed(
             title=user,
             colour=discord.Colour.gold()
         )
-        # creating profile embed
         profile_embed.set_thumbnail(url=icon)
         wr = int((wins/(wins+losses))*1000)/10
         profile_embed.add_field(name="Rank:", value=f"{tier} {rank}, LP: {lp}")
         profile_embed.add_field(name="Win/Loss:", value=f"W: {wins}, L: {losses}, WR: {wr}%", inline=False)
-        await ctx.send(embed=profile_embed)
+        await ctx.send(embed=profile_embed)'''
 
 
 # add cog
