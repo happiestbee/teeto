8c83c504008126b94c24a24cd8d89541406f90df
happiestbee <87628038+happiestbee@users.noreply.github.com>
2021-08-27 03:33:30 -0400

    Deleted items sectoin, started stats and abilities

--- cogs/champion.py
+++ cogs/champion.py
@@ -40,8 +40,34 @@ async def champion(champion_query: cass.Champion):
 
 
 # get stat information
-async def get_stats(champion_query: cass.Champion):
-    pass
+async def get_stats(champion_query: cass.Champion, embed: discord.Embed):
+    stats = champion_query.stats
+    wr = champion_query.info
+    diff = wr.difficulty
+
+    # edit embed message
+    embed.clear_fields()
+    embed.add_field(name="Difficulty:", value=f"{diff}/10")
+    return embed
+
+
+# get ability information
+async def get_abilities(champion_query: cass.Champion, embed: discord.Embed):
+    # create dict for ability information
+    abilities = {}
+
+    # get passive information
+    passive = champion_query.passive
+    abilities[0] = (passive.name, passive.description, passive.image_info.url)
+
+    # get regular abilities
+    i = 1
+    for spell in champion_query.spells:
+        abilities[i] = (spell.name, spell.description, spell.image_info.url)
+        i += 1
+
+    print(abilities)
+    return abilities
 
 
 # get skins information
@@ -85,30 +111,34 @@ class SkinView(discord.ui.View):
 
 # create champion view for buttons
 class ChampionView(discord.ui.View):
-    def __init__(self, ctx, message, champ):
+    def __init__(self, ctx, message, champ, embed):
         super().__init__()
 
         # get message to edit
         self.message = message
+        self.embed = embed
 
         self.champ = champ
         self.ctx = ctx
 
     # instantiate buttons
     @discord.ui.button(label="Overview",
-                       style=discord.ButtonStyle.green,
-                       disabled=False,
+                       style=discord.ButtonStyle.gray,
+                       disabled=True,
                        emoji='❔')
     async def overview(self, button: discord.ui.Button, interaction: discord.Interaction):
         for buttons in self.children:
             if buttons != button:
                 buttons.disabled = False
                 buttons.style = discord.ButtonStyle.green
+
+        overview_embed = await champion(self.champ)
+
         button.disabled = True
         button.style = discord.ButtonStyle.gray
 
         # update message
-        await self.message.edit(view=self)
+        await self.message.edit(embed=overview_embed, view=self)
 
     @discord.ui.button(label="Stats",
                        style=discord.ButtonStyle.green,
@@ -122,23 +152,10 @@ class ChampionView(discord.ui.View):
         button.disabled = True
         button.style = discord.ButtonStyle.gray
 
-        # update message
-        await self.message.edit(view=self)
-
-    @discord.ui.button(label="Items",
-                       style=discord.ButtonStyle.green,
-                       disabled=False,
-                       emoji='🛒')
-    async def items(self, button: discord.ui.Button, interaction: discord.Interaction):
-        for buttons in self.children:
-            if buttons != button:
-                buttons.disabled = False
-                buttons.style = discord.ButtonStyle.green
-        button.disabled = True
-        button.style = discord.ButtonStyle.gray
+        stats_embed = await get_stats(self.champ, self.embed)
 
         # update message
-        await self.message.edit(view=self)
+        await self.message.edit(embed=stats_embed, view=self)
 
     @discord.ui.button(label="Abilities",
                        style=discord.ButtonStyle.green,
@@ -149,6 +166,11 @@ class ChampionView(discord.ui.View):
             if buttons != button:
                 buttons.disabled = False
                 buttons.style = discord.ButtonStyle.green
+
+        abilities = await get_abilities(self.champ, self.embed)
+        for index, ability in abilities.items():
+            await self.ctx.send(content=f"{ability[0]}, {ability[1]}\n{ability[2]}")
+
         button.disabled = True
         button.style = discord.ButtonStyle.gray
 
@@ -200,7 +222,7 @@ class Champion(commands.Cog):
             msg = await ctx.send(embed=e)
 
             # create view for message with buttons
-            view = ChampionView(ctx, msg, champ)
+            view = ChampionView(ctx, msg, champ, e)
             await msg.edit(embed=e, view=view)
         # check if champion exists
         except Exception as e:
